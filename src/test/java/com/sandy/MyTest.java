6f7f5e761fd46aabbccc4850a0cf85ce6ba9aadf
Sandeep Singh Negi (RBEI/BSW2) <end1cob@bosch.com>
2020-10-03 10:28:28 +0530

    second commit

--- src/test/java/com/sandy/MyTest.java
+++ src/test/java/com/sandy/MyTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
@@ -25,10 +26,9 @@ public class MyTest {
 		System.out.println("Ebdug test");
 	}
 
-	
 	public static void setChromeDriverPath() {
 		String chromedriverPath = System.getProperty("user.dir")
-				+ "\\src\\test\\resources\\executable\\chromedriver.exe";
+				+ "\\src\\main\\resources\\executable\\chromedriver.exe";
 
 		System.setProperty("webdriver.chrome.driver", chromedriverPath);
 
@@ -36,37 +36,39 @@ public class MyTest {
 
 	public static void setFirefoxDriverPath() {
 		String firefoxdriverpath = System.getProperty("user.dir")
-				+ "\\src\\test\\resources\\executable\\geckodriver.exe";
+				+ "\\src\\main\\resources\\executable\\geckodriver.exe";
 
 		System.setProperty("webdriver.gecko.driver", firefoxdriverpath);
 
 	}
+
 	@Test
 	public void testingTitle() throws MalformedURLException {
 
-		
-		String rootDir=System.getProperty("user.dir");
-		String firefoxPath=rootDir+"//src//test//resources//geckodriver.exe";
+		String rootDir = System.getProperty("user.dir");
+		String firefoxPath = rootDir + "//src//test//resources//geckodriver.exe";
 
 		/*
 		 * Capabilities chromeCapabilities = DesiredCapabilities.chrome(); Capabilities
 		 * firefoxCapabilities = DesiredCapabilities.firefox();
 		 */
 
-		
-		 setChromeDriverPath();
-		 setFirefoxDriverPath();
-		//  WebDriver chrome=new ChromeDriver(); 
-		  WebDriver firefox=new FirefoxDriver();
-		 
-			/*
-			 * RemoteWebDriver chrome = new RemoteWebDriver(new
-			 * URL("http://18.188.162.146:4444/wd/hub"), chromeCapabilities);
-			 * RemoteWebDriver firefox = new RemoteWebDriver(new
-			 * URL("http://18.188.162.146:4444/wd/hub"), firefoxCapabilities);
-			 */
+		setChromeDriverPath();
+		setFirefoxDriverPath();
+		// WebDriver chrome=new ChromeDriver();
 
-		  
+		FirefoxOptions opts = new FirefoxOptions();
+		opts.addArguments("-private");
+		opts.setHeadless(true);
+		WebDriver firefox = new FirefoxDriver();
+
+		/*
+		 * RemoteWebDriver chrome = new RemoteWebDriver(new
+		 * URL("http://18.188.162.146:4444/wd/hub"), chromeCapabilities);
+		 * RemoteWebDriver firefox = new RemoteWebDriver(new
+		 * URL("http://18.188.162.146:4444/wd/hub"), firefoxCapabilities);
+		 */
+
 		// run against chrome
 		/*
 		 * chrome.get("https://www.google.in"); System.out.println(chrome.getTitle());
@@ -76,7 +78,7 @@ public class MyTest {
 		firefox.get("https://www.google.in");
 		System.out.println(firefox.getTitle());
 
-	//	chrome.quit();
+		// chrome.quit();
 		firefox.quit();
 
 	}
